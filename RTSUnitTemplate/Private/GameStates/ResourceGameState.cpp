229b9059ba201730b703b46a8f64ba27ea0253dd
SilvanTeufel <teufelsilvan@gmail.com>
2026-01-01 19:25:38 +0100

    Little change

--- RTSUnitTemplate/Private/GameStates/ResourceGameState.cpp
+++ RTSUnitTemplate/Private/GameStates/ResourceGameState.cpp
@@ -19,31 +19,4 @@ void AResourceGameState::OnRep_TeamResources()
 void AResourceGameState::SetTeamResources(TArray<FResourceArray> Resources)
 {
 	TeamResources = Resources;
-
-	/*
-	// 1. We must get the Enum object here to convert the enum value to a string.
-	const UEnum* EnumPtr = FindObject<UEnum>(ANY_PACKAGE, TEXT("EResourceType"), true);
-	if (!EnumPtr)
-	{
-		// Safety check in case the enum can't be found.
-		return;
-	}
-
-	UE_LOG(LogTemp, Warning, TEXT("--- GameState Updated: Displaying All Team Resources ---"));
-
-	// 2. Loop through each resource type (e.g., Primary, Secondary).
-	for (const FResourceArray& Data : TeamResources)
-	{
-		FString ResourceName = EnumPtr->GetValueAsString(Data.ResourceType);
-
-		// 3. To get the TeamId, we loop through the 'Resources' array inside the struct.
-		// The index of this array corresponds to the TeamId.
-		for (int32 TeamIndex = 0; TeamIndex < Data.Resources.Num(); ++TeamIndex)
-		{
-			float Amount = Data.Resources[TeamIndex];
-			UE_LOG(LogTemp, Warning, TEXT("   Team %d -> %s = %.1f"), TeamIndex, *ResourceName, Amount);
-		}
-	}
-	// --- End of Debugging Log ---
-	*/
 }
